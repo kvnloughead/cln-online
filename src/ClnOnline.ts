f7695de9cbcf2b4434c957825a60b7f3759e20d3
kvnloughead <kvnloughead@gmail.com>
2022-04-29 19:09:04 -0400

    fix formatting and eslint configuration and remove default markup and styles
     - fix lit-html template formatting
     - make eslint ignore that which it should

--- src/ClnOnline.ts
+++ src/ClnOnline.ts
@@ -1,10 +1,8 @@
 import { LitElement, html, css } from 'lit';
 import { property } from 'lit/decorators.js';
 
-const logo = new URL('../../assets/open-wc-logo.svg', import.meta.url).href;
-
 export class ClnOnline extends LitElement {
-  @property({ type: String }) title = 'My app';
+  @property({ type: String }) title = 'CLN Online';
 
   static styles = css`
     :host {
@@ -20,61 +18,13 @@ export class ClnOnline extends LitElement {
       text-align: center;
       background-color: var(--cln-online-background-color);
     }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
-    }
   `;
 
   render() {
     return html`
       <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
         <h1>${this.title}</h1>
-
-        <p>Edit <code>src/ClnOnline.ts</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
       </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
     `;
   }
 }
